a910ab57af88bf9048eb753fa45a50a600b8f1f3
Raimonds Kazuss <rkazush@gmail.com>
2022-04-06 20:23:39 +0300

    UI layout for slider

--- src/components/Slider/Slider.tsx
+++ src/components/Slider/Slider.tsx
@@ -1,10 +1,49 @@
-const Slider:React.FC = () => {
-    return (
-        <div>
-            <div>slider image</div>
-            <div>slider buttons</div>
-        </div>
-    )
-}
+import styled from "styled-components";
+import sliderImage from "../../assets/duck-1.jpeg";
 
-export default Slider
+const SliderWrapper = styled.div`
+  padding: 0.5rem;
+  margin: auto;
+  width: 100%;
+  max-width: 64rem;
+  height: 11rem;
+  box-sizing: border-box;
+`;
+
+const Image = styled.div`
+  width: 100%;
+  height: 8rem;
+  background: #dbe2ef url(${sliderImage}) center/cover no-repeat;
+`;
+
+const DotWrapper = styled.div`
+  height: 2rem;
+  display: flex;
+  width: 10rem;
+  margin: 0.5rem auto;
+  justify-content: space-between;
+`;
+
+const Dot = styled.div`
+  width: 1rem;
+  height: 1rem;
+  border-radius: 50%;
+  background: #DBE2EF;
+`;
+
+const Slider: React.FC = () => {
+  return (
+    <SliderWrapper>
+      <Image />
+      <DotWrapper>
+        <Dot />
+        <Dot />
+        <Dot />
+        <Dot />
+        <Dot />
+      </DotWrapper>
+    </SliderWrapper>
+  );
+};
+
+export default Slider;
