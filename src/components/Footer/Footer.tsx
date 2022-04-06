b47d13d83ede0b968764a48e17a622cd5f4d2e67
Raimonds Kazuss <rkazush@gmail.com>
2022-04-06 20:54:49 +0300

    footer UI layout

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,19 +1,47 @@
+import styled from "styled-components";
+
+const StyledFooter = styled.footer`
+  width: 100%;
+  height: 6rem;
+  position: absolute;
+  bottom: 0;
+  text-align: center;
+`;
+
+const Icons = styled.div`
+  width: 8rem;
+  height: 2rem;
+  margin: 1rem auto;
+  display: flex;
+  justify-content: space-between;
+`;
+
+const Icon = styled.a`
+  width: 2rem;
+  height: 2rem;
+
+  img {
+    width: 2rem;
+    height: 2rem;
+  }
+`;
+
 const Footer: React.FC = () => {
   return (
-    <footer>
-      <div>
-        <a href="/fb" target="_blank">
+    <StyledFooter>
+      <Icons>
+        <Icon href="/fb" target="_blank">
           <img src={require("../../assets/fb-logo.png")} alt="" />
-        </a>
-        <a href="/tw" target="_blank">
-          <img src={require("../../assets/fb-logo.png")} alt="" />
-        </a>
-        <a href="/inst" target="_blank">
-          <img src={require("../../assets/fb-logo.png")} alt="" />
-        </a>
-      </div>
+        </Icon>
+        <Icon href="/tw" target="_blank">
+          <img src={require("../../assets/tw-logo.png")} alt="" />
+        </Icon>
+        <Icon href="/inst" target="_blank">
+          <img src={require("../../assets/inst-logo.png")} alt="" />
+        </Icon>
+      </Icons>
       <div>© 2022 Raimonds Kazušs</div>
-    </footer>
+    </StyledFooter>
   );
 };
 
